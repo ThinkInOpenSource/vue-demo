447400fe5f6d21a48f6f632b41847ab80316886e
luoxn28 <luoxn28@163.com>
2018-05-19 17:19:02 +0800

    引入element-ui

--- src/main.js
+++ src/main.js
@@ -12,6 +12,10 @@ Vue.use(globalJs)
 import axios from 'axios'
 Vue.prototype.$http = axios
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
